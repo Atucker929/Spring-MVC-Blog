58f44ca48f64abe9dc68a8a6a84a2a4d4ac661a7
moeli <weeshallovercome@yahoo.com>
2019-12-17 13:35:16 -0500

    Initial file layout and views

---
+++ src/main/java/blog/BlogMvcApplication.java
@@ -0,0 +1,16 @@
+package blog;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class BlogMvcApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogMvcApplication.class, args);
+    }
+}
